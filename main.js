beebfde98f9c6ea1aed276c9b455883535ef9ff6
mas0110 <masoudmohammed13@gmail.com>
2023-12-22 16:23:42 +0100

    prezzo del biglietto

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
+// kilometri che deve percorrere e la sua età
 let numerokm = prompt("quanti kilometri devi fare?:")
 
 let età = prompt("quanti anni hai?:")
 
 console.log( numerokm + " "+ età)
+
+// prezzo totale del biglietto
+console.log(numerokm * 0.21)
