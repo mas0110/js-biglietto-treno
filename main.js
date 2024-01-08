6669005eda07134bfb6d40e78d44eaf5bbdfd5eb
mas0110 <masoudmohammed13@gmail.com>
2024-01-08 09:18:46 +0100

    prezzo finale con sconti

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
 // kilometri che deve percorrere e la sua età
-let numerokm = prompt("quanti kilometri devi fare?:")
+let numerokm = prompt("Quanti kilometri devi fare?:")
 
-let età = prompt("quanti anni hai?:")
+let età = prompt("Quanti anni hai?:")
 
 console.log( numerokm + " "+ età)
 
 // prezzo totale del biglietto
-console.log(numerokm * 0.21)
+
+var prezzokm = 0.21;
+
+console.log(numerokm * prezzokm)
+
+var prezzo = numerokm * prezzokm;
+
+
+// prezzo totale del biglietto con sconti applicati
+
+var prezzo20 = prezzo - prezzo / 100 * 20;
+
+var prezzo40 = prezzo - prezzo / 100 * 40;
+
+if (età < 18) {
+  document.getElementById("finalprice").innerHTML = 'Il prezzo del suo biglietto è di ' + prezzo20.toFixed(2) + '€ con sconto del 20% per under 18';
+  console.log('Il prezzo è ' + prezzo20.toFixed(2) + '€ con sconto del 20% per under 18');
+}
+else if (età > 64) {
+  document.getElementById("finalprice").innerHTML = 'Il prezzo del suo biglietto è di ' + prezzo40.toFixed(2) + ' con sconto del 40% per over 65';
+  console.log('Il prezzo è ' + prezzo40.toFixed(2) + '€  con sconto del 40% per over 65');
+}
+else {
+  document.getElementById("finalprice").innerHTML = 'Il prezzo del suo biglietto è di ' + prezzo.toFixed(2) + '€';
+  console.log('Il prezzo è ' + prezzo.toFixed(2));
+}
