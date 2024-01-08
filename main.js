2fba1469d251dd766bc1644aa86e0123582b9f02
mas0110 <masoudmohammed13@gmail.com>
2024-01-08 10:15:39 +0100

    correzione maggiori uguali

--- main.js
+++ main.js
@@ -20,11 +20,11 @@ let prezzo20 = prezzo - prezzo / 100 * 20;
 
 let prezzo40 = prezzo - prezzo / 100 * 40;
 
-if (età < 18) {
+if (età <= 18) {
   document.getElementById("finalprice").innerHTML = 'Il prezzo del suo biglietto è di ' + prezzo20.toFixed(2) + '€ con sconto del 20% per under 18';
   console.log('Il prezzo è ' + prezzo20.toFixed(2) + '€ con sconto del 20% per under 18');
 }
-else if (età > 64) {
+else if (età >= 64) {
   document.getElementById("finalprice").innerHTML = 'Il prezzo del suo biglietto è di ' + prezzo40.toFixed(2) + ' con sconto del 40% per over 65';
   console.log('Il prezzo è ' + prezzo40.toFixed(2) + '€  con sconto del 40% per over 65');
 }
